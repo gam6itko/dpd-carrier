9c7f93bbf55e07cb4254228351bb3a9c3c71ddd8
gam6itko <gam6itko@gmail.com>
2017-08-11 22:02:45 +0300

    Address getter and setter add

--- src/Gam6itko/DpdCarrier/Type/Geography/Address.php
+++ src/Gam6itko/DpdCarrier/Type/Geography/Address.php
@@ -27,4 +27,148 @@ class Address extends AbstractDeliveryPoint
     protected $ownership;
     /** @var string */
     protected $descript;
+
+    /**
+     * @return string
+     */
+    public function getRegionName()
+    {
+        return $this->regionName;
+    }
+
+    /**
+     * @param string $regionName
+     * @return Address
+     */
+    public function setRegionName($regionName)
+    {
+        $this->regionName = $regionName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreet()
+    {
+        return $this->street;
+    }
+
+    /**
+     * @param string $street
+     * @return Address
+     */
+    public function setStreet($street)
+    {
+        $this->street = $street;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreetAbbr()
+    {
+        return $this->streetAbbr;
+    }
+
+    /**
+     * @param string $streetAbbr
+     * @return Address
+     */
+    public function setStreetAbbr($streetAbbr)
+    {
+        $this->streetAbbr = $streetAbbr;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHouseNo()
+    {
+        return $this->houseNo;
+    }
+
+    /**
+     * @param string $houseNo
+     * @return Address
+     */
+    public function setHouseNo($houseNo)
+    {
+        $this->houseNo = $houseNo;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBuilding()
+    {
+        return $this->building;
+    }
+
+    /**
+     * @param string $building
+     * @return Address
+     */
+    public function setBuilding($building)
+    {
+        $this->building = $building;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStructure()
+    {
+        return $this->structure;
+    }
+
+    /**
+     * @param string $structure
+     * @return Address
+     */
+    public function setStructure($structure)
+    {
+        $this->structure = $structure;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOwnership()
+    {
+        return $this->ownership;
+    }
+
+    /**
+     * @param string $ownership
+     * @return Address
+     */
+    public function setOwnership($ownership)
+    {
+        $this->ownership = $ownership;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescript()
+    {
+        return $this->descript;
+    }
+
+    /**
+     * @param string $descript
+     * @return Address
+     */
+    public function setDescript($descript)
+    {
+        $this->descript = $descript;
+        return $this;
+    }
 }
