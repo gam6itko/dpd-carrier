4745276ff4d5337bdf5274abad214560ea4a87dd
gambit <gam6itko@gmail.com>
2017-10-24 12:30:33 +0300

    ServiceCode getDescriptions

--- src/Gam6itko/DpdCarrier/Enum/ServiceCode.php
+++ src/Gam6itko/DpdCarrier/Enum/ServiceCode.php
@@ -5,25 +5,35 @@ final class ServiceCode
 {
     /** DPD 18:00 */
     const BZP = 'BZP';
-
     /** DPD ECONOMY */
     const ECN = 'ECN';
-
     /** DPD CLASSIC */
     const CUR = 'CUR';
-
     /** DPD EXPRESS */
     const NDY = 'NDY';
-
     /** DPD Online Express */
     const CSM = 'CSM';
-
     /** DPD Online Classic */
     const PCL = 'PCL';
-
     /** DPD Classic international */
     const DPI = 'DPI';
-
     /** DPD MAX domestic */
     const MAX = 'MAX';
+
+    /**
+     * @return array
+     */
+    public static function getDescriptions()
+    {
+        return [
+            'BZP' => 'DPD 18:00',
+            'ECN' => 'DPD ECONOMY',
+            'CUR' => 'DPD CLASSIC',
+            'NDY' => 'DPD EXPRESS',
+            'CSM' => 'DPD Online Express',
+            'PCL' => 'DPD Online Classic',
+            'DPI' => 'DPD Classic international',
+            'MAX' => 'DPD MAX domestic'
+        ];
+    }
 }
