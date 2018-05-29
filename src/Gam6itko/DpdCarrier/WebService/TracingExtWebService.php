c88031351de58b58291e04aac810ac4d0a73af60
gambit <gam6itko@gmail.com>
2018-05-29 12:34:41 +0300

    TracingExtWebService

---
+++ src/Gam6itko/DpdCarrier/WebService/TracingExtWebService.php
@@ -0,0 +1,29 @@
+<?php
+namespace Gam6itko\DpdCarrier\WebService;
+
+class TracingExtWebService extends TracingWebService
+{
+    /**
+     * @return mixed
+     */
+    protected function getWsdlTest()
+    {
+        return 'http://wstest.dpd.ru/services/tracing1-1?wsdl';
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getWsdlProd()
+    {
+        return 'http://ws.dpd.ru/services/tracing1-1?wsdl';
+    }
+
+    /**
+     * @throws \LogicException
+     */
+    public function getStatesByClient()
+    {
+        throw new \LogicException('Unsupported');
+    }
+}
